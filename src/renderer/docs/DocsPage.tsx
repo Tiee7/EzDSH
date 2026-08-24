caef01d60dec32ac8748d790380c7e859f85e05e
Tiee <tieetheai@gmail.com>
2026-08-24 23:05:23 +0800

    fix: update zh docs page link to Windows install section

--- src/renderer/docs/DocsPage.tsx
+++ src/renderer/docs/DocsPage.tsx
@@ -1,7 +1,7 @@
 import type { AppLocale } from '../../shared/locale.js'
 
 const DOCS_FAQ_URLS: Record<AppLocale, string> = {
-  zh: 'https://docs.ezdsh.com/faq',
+  zh: 'https://docs.ezdsh.com/guide/install#windows-%E5%AE%89%E8%A3%85%E6%AD%A5%E9%AA%A4',
   en: 'https://docs.ezdsh.com/en/faq'
 }
 
